c2b49674def6ff2c4fbd9ef4c07af48a8b844793
geekynice <nicebanjaraa@gmail.com>
2024-03-30 17:47:33 -0400

    ajax setup

--- oasis_app/views.py
+++ oasis_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def prompt(request):
+    if request.method == 'POST':
+        prompt1 = request.POST.get('prompt1')
+        feeling = request.POST.get('feeling')
+        stress_level = request.POST.get('stress_level')
+        
+        result = {
+            'prompt1': prompt1,
+            'feeling': feeling,
+            'stress_level': stress_level,
+            'dummy_response': f'{prompt1}, {feeling} and {stress_level}.'
+        }
+        return JsonResponse(result)
