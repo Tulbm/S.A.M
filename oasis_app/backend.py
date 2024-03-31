9150af48b2ce934d4ee2b0f512996a118adaf8be
geekynice <nicebanjaraa@gmail.com>
2024-03-30 21:21:33 -0400

    test

--- oasis_app/backend.py
+++ oasis_app/backend.py
@@ -75,36 +75,42 @@ def topics(text):
 import requests
 import time
 
-def predict(text, feeling, stress_level, loaded=False):
+import aiohttp
+import asyncio
+
+
+async def predict(text, feeling, stress_level, loaded=False):
     API_KEY = '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'
     MODEL_ID = 'DaJulster/Mental_health_identification'
     API_URL = f"https://api-inference.huggingface.co/models/{MODEL_ID}"
     input_text = f"I am feeling: {feeling}. {text}" 
     headers = {"Authorization": f"Bearer {API_KEY}"}
 
-    def query(payload, url, headers):
-        response = requests.post(url, json=payload, headers=headers)
-        response.raise_for_status()
-        return response.json()
+    async with aiohttp.ClientSession() as session:
+        async def query(payload, url, headers):
+            async with session.post(url, json=payload, headers=headers) as response:
+                response.raise_for_status()
+                return await response.json()
 
-    try:
-        output = query({"inputs": input_text}, API_URL, headers)
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        time.sleep(10)
-        output = query({"inputs": input_text}, API_URL, headers)
+        try:
+            output = await query({"inputs": input_text}, API_URL, headers)
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            await asyncio.sleep(10)
+            output = await query({"inputs": input_text}, API_URL, headers)
 
-    score = None
-    for item in output[0]:
-        if item['label'] == 'Negative':
-            score = item['score']
+        score = None
+        for item in output[0]:
+            if item['label'] == 'NEGATIVE':
+                score = item['score']
 
-    if score is not None:
-        score += 0.08 * stress_level
-    print('---------DEBUG------------')
-    print("Score as", score)
-    print('---------DEBUG------------')
-    return score
+        if score is not None:
+            score += 0.08 * stress_level
+        print('---------DEBUG------------')
+        print("Score as", score)
+        print('---------DEBUG------------')
+        return score
+        
     
 
 
