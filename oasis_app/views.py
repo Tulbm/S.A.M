45c2202efeedcc83976ddd32aa9e5fe7c5f4185a
geekynice <nicebanjaraa@gmail.com>
2024-03-31 09:14:18 -0400

    final changes

--- oasis_app/views.py
+++ oasis_app/views.py
@@ -17,26 +17,6 @@ async def main(prompt1, feel):
 def index(request):
     return render(request, 'index.html')
 
-async def analyze_data(prompt1, feeling, stress_level):
-    score = test.predict(prompt1, feeling, stress_level)
-    topics = test.extract_topics(prompt1)
-    await score
-    print(score)
-    if score is not None and score >= 0.6:
-        response = await test.generate_response(text=prompt1, postive=False)
-    else:
-        prompt1 = "You are interested in: " + ", ".join(topics) + ". " + prompt1
-        response = await test.generate_response(text=prompt1, postive=True)
-    
-    result = {
-        'prompt1': prompt1,
-        'feeling': feeling,
-        'stress_level': stress_level,
-        'dummy_response': response
-    }
-    
-    return await result
-
 def prompt(request):
     if request.method == 'POST':
         prompt1 = request.POST.get('prompt1')
@@ -57,16 +37,35 @@ def prompt(request):
             print("Result:", result)
         asyncio.run(test_predict())
 
-        result = asyncio.run(analyze_data(prompt1, feel, stress))
+        result = asyncio.run(main(prompt1, feel))
         response_data = {
             'prompt1': prompt1,
             'feel': feel,
             'stress': stress,
             'result': result,
-            'dummy_response': f'{prompt1}, {feel} and {stress}. | {result}'
         }
         print(response_data['result'])
         time.sleep(1)
         return JsonResponse(response_data)
     else:
         return render(request, 'index.html')
+    
+async def analyze_data(prompt1, feeling, stress_level):
+    score = backend.predict(prompt1, feeling, stress_level)
+    topics = backend.topics(prompt1)
+    await score
+    print(score)
+    if score is not None and score >= 0.6:
+        response = await backend.generate_bad(prompt1)
+    else:
+        response = "You are interested in: " + ", ".join(topics)
+    
+    result = {
+        'prompt1': prompt1,
+        'feeling': feeling,
+        'stress_level': stress_level,
+        'dummy_response': response
+    }
+    
+    return await result
+
