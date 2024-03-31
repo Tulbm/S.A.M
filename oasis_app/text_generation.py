a89cc289efa179c952bdb4a70f774c5f62301efc
Julien-ser <julien.serbanescu@gmail.com>
2024-03-31 08:47:43 -0400

    Update text_generation.py

--- oasis_app/text_generation.py
+++ oasis_app/text_generation.py
@@ -10,6 +10,7 @@ async def generate_bad(text, feeling):
 
     async with aiohttp.ClientSession() as session:
         async def query(payload, url, headers):
+            payload["parameters"] = {"max_length": 150}
             async with session.post(url, json=payload, headers=headers) as response:
                 response.raise_for_status()
                 return await response.json()
